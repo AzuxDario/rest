7deed37c18924f393b5457a182e0914b9ba2db96
AzuxDario <pawel_mibor95@tlen.pl>
2018-01-25 18:47:41 +0100

    Add code to server

--- server/server.php
+++ server/server.php
@@ -1,7 +1,16 @@
 <?php
+
+const DbServer = "localhost";
+const DbUser = "root";
+const DbPassword = "";
+const DbName = "myBase";
+
 //get HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
 $request = explode('/', trim($_SERVER['PATH_INFO','/']));
 $input = json_decode(file_get_contents('php://input'),true);
+
+//connect do database
+$connection = mysqli_connect(DbServer, DbUser, DbPassword, DbName);
 
 ?>
