ebe445c6785354fe4b0170837fb7f86761a04088
AzuxDario <pawel_mibor95@tlen.pl>
2018-01-29 22:49:59 +0100

    Add requests

--- server/BooksController.php
+++ server/BooksController.php
@@ -2,29 +2,29 @@
 
 class BooksController
 {
-	public function GetBook($id)
+	public function GetBook($params)
 	{
-		
+		$sqlCommand = "SELECT * FROM books WHERE id = " . $params['id']  . ";";
 	}
 	
 	public function GetBooks()
 	{
-		
+		$sqlCommand = "SELECT * FROM books;";
 	}
 	
-	public function AddBook()
+	public function AddBook($params, $request)
 	{
-		
+		$sqlCommand = "INSERT INTO books(title, author, isbn) VALUES ('" .  $request['title'] ."', '" . $request['author'] . "', '" . $request['isbn'] ."');";
 	}
 	
-	public function UpdateBook()
+	public function UpdateBook($params, $request)
 	{
-		
+		$sqlCommand = "UPDATE books SET title = '" .  $request['title'] ."', author = '" . $request['author'] . "', isbn = '" . $request['isbn'] ." WHERE " . $params['id'] . ";";
 	}
 	
-	public function DeleteBook()
+	public function DeleteBook($params)
 	{
-		
+		$sqlCommand = "DELETE FROM books WHERE id = " . $params['id']  . ";";
 	}
 }
 
