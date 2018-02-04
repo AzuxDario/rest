e120d570f4f06d160f5d7bb9c6daa332c76cac9a
AzuxDario <pawel_mibor95@tlen.pl>
2018-02-04 23:02:25 +0100

    GET working

--- server/Router.php
+++ server/Router.php
@@ -3,11 +3,11 @@ require_once('BooksController.php');
 require_once('AltoRouter.php');
 $router = new AltoRouter();
 $router->setBasePath('/rest/server');
-$router->map('GET','/books/', 'BooksController::GetBooks');
-$router->map('GET','/books/[i:id]/', 'BooksController::GetBook');
-$router->map('POST','/books/', 'BooksController::AddBook');
-$router->map('PUT','/books/[i:id]/', 'BooksController::UpdateBook');
-$router->map('DELETE','/books/[i:id]/', 'BooksController::DeleteBook');
+$router->map('GET','/books', 'BooksController::GetBooks');
+$router->map('GET','/books/[i:id]', 'BooksController::GetBook');
+$router->map('POST','/books', 'BooksController::AddBook');
+$router->map('PUT','/books/[i:id]', 'BooksController::UpdateBook');
+$router->map('DELETE','/books/[i:id]', 'BooksController::DeleteBook');
 // match current request
 $match = $router->match();
 // call closure or throw 404 status
@@ -22,7 +22,7 @@ if( $match && is_callable( $match['target'] ) )
 else
 {
 	// no route was matched
-	$response = array('status' => array( 'code' => 404, 'message' => 'Not Found'), 'data' => $result);
+	$response = array('status' => array( 'code' => 404, 'message' => 'Not Found'), 'data' => null);
 	
 	echo json_encode($response);
 }
