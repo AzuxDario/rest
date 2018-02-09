fc8307341e95626eddcd111ff9a3c3adcab7074e
AzuxDario <pawel_mibor95@tlen.pl>
2018-02-09 19:46:23 +0100

    add tabs

--- view/controllers/editBookController.js
+++ view/controllers/editBookController.js
@@ -1,12 +1,12 @@
 app.controller('EditBookController', ['$scope', '$routeParams', 'BookFactory', '$location' ,function ($scope, $routeParams, BookFactory, $location) {
     
     $scope.editBook = function () {
-      BookFactory.update($scope.response.data[0]);
-      $location.path('/books/');
+		BookFactory.update($scope.response.data[0]);
+		$location.path('/books/');
     };
 
     $scope.cancel = function () {
-      $location.path('/books/');
+		$location.path('/books/');
 	};
 	
 	$scope.response = BookFactory.show({id: $routeParams.id});
